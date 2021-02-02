a479d2900ef83d5dba83354e07fe23dafdad71cd
Susan Ramos <69052137+Sua027@users.noreply.github.com>
2021-02-01 23:19:57 -0600

    Add files via upload

---
+++ js/ejercicio1.js
@@ -0,0 +1,28 @@
+//IF DENTRO DE OTRO IF
+
+//var precio = prompt("precio")
+
+//if(precio >= 100){
+    
+//    var condescuento = precio - (precio*10/100);
+    
+//    aler("Q" + precio + "tiene un 10% de descuento total a pagar    Q" + condescuento)
+          
+//}else{
+//          alert("precio completo")
+//          }
+
+//var user = prompt("Usuario:")
+
+//if(user == "jefuentes"){
+    
+ //   var password = prompt("Contraseña")
+    
+  //  if(password == "gatitos123"){
+  //     alert("hola " + user)
+  //  }else{
+//        alert("Contraseña incorrecta")
+   // }
+//}else{
+//    alert("Usuario no encontrado")
+//}s
---
+++ js/programación.js
@@ -0,0 +1,41 @@
+//ESTO SE UTILIZA PARA DESPLEGAR UNA VENTANA 
+//EMERGENTE CON UN MENSAJE.
+
+    //alert("ya estas conectado con Javascript");
+
+//VARIABLES CON TIPO DE DATOS
+//var datoTexto = "hola"      //dato de tipo cadena de texto
+//var datoNumerico = 2021     //dato numerico
+//var datoDecimal=3.1416      //dato desimal
+//var datoChar = 'a'          //dato de tipo caracter
+//var datosLogicos = true     //o false
+
+//UTILIZANDO UN ALERT PARA MOSTRAR LOS TIPOS DE DATOS
+//CON VARIABLES UNIDOS CON EL SIGNOS+
+//alert( datoTexto+" com estas "+ datoNumerico)
+//OPERACIÓN ENTRE TIPOS DE DATOS COMPATIBLES.
+//alert(datoNumerico * datoDesimal)
+
+//<
+//>=
+//>
+//>=
+//==
+//!=
+
+//var edad =prompt("cual es tu edad"); 
+
+//if(edad>=18){
+    //alert("Eres mayor de edad")
+    //}else{
+   // alert("Eres menor de edad") 
+    //}
+    
+    
+    
+    
+    
+    
+    
+    
+    
